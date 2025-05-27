2af795f81907ad5ef7b7ce53223b599052eaf689
Madara0330E <summoners2011sss@gmail.com>
2025-05-27 22:36:05 +0500

    updated the version and added the label a home
    updated the version and added the label a home

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,48 +1,48 @@
-import 'react-image-gallery/styles/scss/image-gallery.scss';
-import './Gallery.scss';
-import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+import "./Gallery.scss";
+import ImageGallery from "react-image-gallery";
 
-import objectPicOne from '../../assets/images/apartaments/image-47.jpg';
-import objectPicTwo from '../../assets/images/apartaments/image-48.jpg';
-import objectPicThree from '../../assets/images/apartaments/image-49.jpg';
-import objectPicFour from '../../assets/images/apartaments/image-50.jpg';
-import objectPicFive from '../../assets/images/apartaments/image-51.jpg';
+import objectPicOne from "../../assets/images/apartaments/image-47.jpg";
+import objectPicTwo from "../../assets/images/apartaments/image-48.jpg";
+import objectPicThree from "../../assets/images/apartaments/image-49.jpg";
+import objectPicFour from "../../assets/images/apartaments/image-50.jpg";
+import objectPicFive from "../../assets/images/apartaments/image-51.jpg";
 
 function Gallery() {
-    const images = [
-        {
-            original: objectPicOne,
-            thumbnail: objectPicOne
-        },
-        {
-            original: objectPicTwo,
-            thumbnail: objectPicTwo
-        },
-        {
-            original: objectPicThree,
-            thumbnail: objectPicThree
-        },
-        {
-            original: objectPicFour,
-            thumbnail: objectPicFour
-        },
-        {
-            original: objectPicFive,
-            thumbnail: objectPicFive
-        }
-    ];
+  const images = [
+    {
+      original: objectPicOne,
+      thumbnail: objectPicOne,
+    },
+    {
+      original: objectPicTwo,
+      thumbnail: objectPicTwo,
+    },
+    {
+      original: objectPicThree,
+      thumbnail: objectPicThree,
+    },
+    {
+      original: objectPicFour,
+      thumbnail: objectPicFour,
+    },
+    {
+      original: objectPicFive,
+      thumbnail: objectPicFive,
+    },
+  ];
 
-    return (
-        <div className="custom-gallery">
-            <ImageGallery
-                items={images}
-                showPlayButton={false}
-                showFullscreenButton={false}
-                slideInterval={1000}
-                slideOnThumbnailOver={false}
-                showIndex={true}
-            />
-        </div>
-    );
+  return (
+    <div className="custom-gallery">
+      <ImageGallery
+        items={images}
+        showPlayButton={false}
+        showFullscreenButton={false}
+        slideInterval={1000}
+        slideOnThumbnailOver={false}
+        showIndex={true}
+      />
+    </div>
+  );
 }
-export { Gallery} ;
+export { Gallery };
